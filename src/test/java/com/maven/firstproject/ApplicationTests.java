710199849ceb2e7af796556e60e85d1acd529625
Abhishek <malmeonabhi@gmail.com>
2023-04-01 03:56:38 +0530

    Adding controllers and Services

--- src/test/java/com/maven/firstproject/ApplicationTests.java
+++ src/test/java/com/maven/firstproject/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.maven.test;
+package com.maven.firstproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
